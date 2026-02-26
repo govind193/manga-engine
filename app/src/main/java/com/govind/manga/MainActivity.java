0a4745eb97ceaabc20e7308c645c81c37ec72476
govind193 <govindasingh811@gmail.com>
2026-02-26 23:11:06 +0530

    Update MainActivity.java

--- app/src/main/java/com/govind/manga/MainActivity.java
+++ app/src/main/java/com/govind/manga/MainActivity.java
@@ -1,1 +1,21 @@
 
+package com.govind.manga;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("Offline AI Manhwa Generator\n\nProject Setup Successful 🚀");
+        textView.setTextSize(20);
+        textView.setPadding(40, 100, 40, 40);
+
+        setContentView(textView);
+    }
+}
