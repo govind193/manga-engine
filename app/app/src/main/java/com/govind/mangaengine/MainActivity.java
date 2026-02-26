8e7d77042568cc10065f1964058fbce58f097846
govind193 <govindasingh811@gmail.com>
2026-02-26 22:22:27 +0530

    add MainActivity

---
+++ app/app/src/main/java/com/govind/mangaengine/MainActivity.java
@@ -0,0 +1,23 @@
+package com.govind.mangaengine;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView web = new WebView(this);
+        setContentView(web);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        web.loadUrl("https://example.com");
+    }
+}
